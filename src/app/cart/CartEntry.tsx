545a352e53388b414ac3e3fd6f5dd712ac7acc7e
MahdiDev75 <149631776+MhdDev75@users.noreply.github.com>
2024-10-10 00:25:46 +0330

    feat: modify cart item

--- src/app/cart/CartEntry.tsx
+++ src/app/cart/CartEntry.tsx
@@ -2,31 +2,58 @@
 
 import { CartItemWithProduct } from "@/lib/db/cart";
 import formatPrice from "@/lib/format";
+import { Minimize, Minus, Plus } from "lucide-react";
 import Link from "next/link";
+import { startTransition, useTransition } from "react";
 
 interface CartEntryProps {
   cartItem: CartItemWithProduct;
+  setProductQuantity: (productId: string, quantity: number) => Promise<void>
 }
 
-const CartEntry = ({ cartItem: { product, quantity } }: CartEntryProps) => {
+const CartEntry = ({ cartItem: { product, quantity }, setProductQuantity }: CartEntryProps) => {
+
+  const [isPending, startTransaction] = useTransition()
   return (
-    <div>
-      <div className="flex flex-wrap items-center gap-3">
+    <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 items-center gap-3  bg-slate-800 rounded-lg p-3">
+      <div className="flex gap-3 col-span-2 items-center">
         <img
           src={product.imageUrl}
           alt={product.name}
-          width={200}
-          height={200}
+          width={120}
+          height={120}
           className="rounded-lg"
         />
-        <Link href={"/products/" + product.id} className="font-bold">
-          {product.name}
-        </Link>
-        <div>قیمت : {formatPrice(Number(product.price ))}</div>
-        <div>تعداد : {quantity}</div>
-        <div> قیمت کل : {formatPrice(Number(product.price * quantity))}</div>
+        <div className="flex flex-col gap-2">
+          <Link href={"/products/" + product.id} className="font-bold">
+            {product.name}
+          </Link>
+          <div className="text-sm text-gray-500">به ازای هر محصول {formatPrice(Number(product.price))} تومان</div>
+
+
+        </div>
       </div>
-      <div className="divider"></div>
+      <div className="flex flex-nowrap gap-2 items-center">
+        <button onClick={() => {
+          const newQuantity = quantity + 1;
+          startTransaction(async () => {
+            await setProductQuantity(product.id, newQuantity)
+          })
+
+        }} className="btn btn-sm btn-success"><Plus size={15} /></button>
+        <input className="input input-bordered w-16 text-center " value={quantity} />
+        <button onClick={() => {
+          const newQuantity = quantity - 1;
+          startTransaction(async () => {
+            await setProductQuantity(product.id, newQuantity < 0 ? 0 : newQuantity)
+          })
+
+        }} className="btn btn-sm btn-primary"><Minus size={15} /></button>
+      </div>
+      <div>  {formatPrice(Number(product.price * quantity))} تومان
+        {isPending && <span className="loading loading-ring loading-sm"></span>}
+
+      </div>
     </div>
   );
 };
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,26 +1,59 @@
 import { getCart } from '@/lib/db/cart'
 import React from 'react'
 import CartEntry from './CartEntry'
+import formatPrice from '@/lib/format'
+import setProductQuantity from './Action'
 
 
 export const metadata = {
-    title:"سبد خرید - مکش مارکت"
+    title: "سبد خرید - مکش مارکت"
 }
 
 const CartPage = async () => {
-    
+
     const cart = await getCart()
 
-  return (
-    <div>
-        <h1 className='mb-6 text-3xl font-bold'>سبد خرید </h1>
-        {
-            cart?.cartItem.map((item)=>(
-                <CartEntry cartItem={item} key={item.id} />
-            ))
-        }
-    </div>
-  )
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3  gap-3'>
+            <div className='col-span-2'>
+                <h1 className='mb-6 text-xl font-bold'>سبد خرید </h1>
+                <div className='flex flex-col gap-4'>
+                    {Number(cart?.cartItem.length) > 0 &&
+                        <div className="hidden  md:grid lg:grid grid-cols-4 items-center gap-3  bg-slate-800 rounded-lg">
+                            <div className='col-span-2 p-2 '>
+                                محصول
+                            </div>
+                            <div>
+                                تعداد
+                            </div>
+                            <div>قیمت</div>
+                        </div>
+                    }
+                    {
+                        cart?.cartItem.map((item) => (
+                            <CartEntry cartItem={item} key={item.id} setProductQuantity={setProductQuantity} />
+                        ))
+                    }
+                </div>
+                {!cart?.cartItem.length && <p className='flex justify-center bg-slate-800 rounded-lg p-3'>سبد خرید شما خالی می‌باشد</p>}
+            </div>
+            <div className="col-span-1">
+                <div className='card bg-slate-800'>
+                    <div className='card-body'>
+                        <span>تعداد محصولات : {cart?.size}</span>
+                        <span>مجموع مبالغ : {formatPrice(Number(cart?.subTotal))} تومان</span>
+                        <span>تخفیف : 0 تومان</span>
+                        <div className='divider' />
+                        <span className='font-bold'>مبلغ قابل پرداخت : {formatPrice(Number(cart?.subTotal))} تومان</span>
+
+
+                        <button className='btn btn-success'>پرداخت</button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default CartPage
